bc7fde1184363694656eaa3321b36c6179707657
aryan-webs <aryankumar2010gmail.com@gmail.com>
2026-05-07 10:46:27 +0530

    Commit changes

--- netlify/functions/pexels.js
+++ netlify/functions/pexels.js
@@ -2,19 +2,33 @@ exports.handler = async function(event) {
 
   const query = event.queryStringParameters.query;
 
-  const response = await fetch(
-    `https://api.pexels.com/videos/search?query=${query}&per_page=5`,
-    {
-      headers: {
-        Authorization: process.env.PEXELS_API_KEY
+  try {
+
+    const response = await fetch(
+      `https://api.pexels.com/videos/search?query=${query}&per_page=5`,
+      {
+        headers: {
+          Authorization: process.env.PEXELS_API_KEY
+        }
       }
-    }
-  );
+    );
 
-  const data = await response.json();
+    const data = await response.json();
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify(data)
-  };
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    };
+
+  } catch (error) {
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: "Failed to fetch videos"
+      })
+    };
+
+  }
+
 };
